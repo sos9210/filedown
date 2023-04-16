b7c75741f246f4a9957d3006fef6dceaa5a6a61b
sungsanghyun <sos9215@gmail.com>
2023-04-16 17:15:11 +0900

    init

---
+++ src/main/java/com/sos/filedown/FiledownApplication.java
@@ -0,0 +1,13 @@
+package com.sos.filedown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiledownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiledownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sos/filedown/FiledownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sos.filedown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiledownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
